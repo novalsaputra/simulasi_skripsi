a153273c1f3475c2850d71cd5ea074b593138061
novalsaputra <novalsaputra0707@gmail.com>
2020-06-03 09:11:59 +0700

    perbaiki simulasi 1

--- simulasi1.py
+++ simulasi1.py
@@ -18,7 +18,7 @@ da = 0.0075
 
 from DeltaTrimax import DeltaTrimax
 a = DeltaTrimax(D)
-ga,ka,wa, msra = a.fit(da,1.2,n_triclusters=100)
+ga,ka,wa, msra = a.fit(da,1.2,n_triclusters=0)
 
 np.savetxt("gen.txt",ga,fmt="%0.f")
 np.savetxt("kondisi.txt",ka,fmt="%0.f")
